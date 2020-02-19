2098f60d17812f742f5195687253374eaccb8b74
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-02-19 21:46:52 +0700

    Remove tmux.conf, moving to byobu

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ ln -sf ${DATA_DIR}/.inputrc ${HOME}/.inputrc
 mkdir -p ${HOME}/.config/nvim
 ln -sf ${DATA_DIR}/init.vim ${HOME}/.config/nvim/init.vim
 
+# private
 mkdir -p ${HOME}/.ssh
 cat ${PRV_DIR}/zasdaym.osh | tee ${HOME}/.ssh/id_rsa
 cat ${PRV_DIR}/zasdaym.pub | tee ${HOME}/.ssh/id_rsa.pub
