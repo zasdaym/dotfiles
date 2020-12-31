9cbf399c4455cd278b26dfa4d296b6cfc9ebb2e5
Zasda Yusuf Mikail <zasdaym@gmail.com>
2020-12-31 14:15:29 +0700

    Add some helpful comments on bootstrap.sh
    Signed-off-by: Zasda Yusuf Mikail <zasdaym@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ set -o errtrace
 set -o nounset
 set -o pipefail
 
+# check required programs
 programs=("curl" "git" "nvim" "zsh")
 for program in ${programs[@]}; do
 	if [[ ! -x "$(command -v ${program})" ]]; then
@@ -13,14 +14,17 @@ for program in ${programs[@]}; do
 	fi
 done
 
+# check shell
 if [[ ${SHELL} != "/bin/zsh" ]]; then
 	chsh -s /bin/zsh
 fi
 
+# check oh-my-zsh
 if [[ ! -d ${HOME}/.oh-my-zsh ]]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
+# symlink config files
 ln -fs "${PWD}/.tmux.conf" "${HOME}/.tmux.conf"
 ln -fsn "${PWD}/nvim" "${HOME}/.config/nvim"
 ln -fs "${PWD}/.zshrc ${HOME}/.zshrc"
