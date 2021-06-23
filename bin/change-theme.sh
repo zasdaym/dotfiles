2038877ccbf1b01a860a8fb68dd1525c67544259
Zasda Yusuf Mikail <zasdaym@gmail.com>
2021-06-23 09:30:12 +0700

    Update change-theme.sh

--- bin/change-theme.sh
+++ bin/change-theme.sh
@@ -3,15 +3,11 @@
 set -o errexit
 set -o nounset
 
-old_theme="gruvbox-dark"
-new_theme="nord"
-
-if [[ $(grep -c nord ~/.config/alacritty/alacritty.yml) -eq 1 ]]; then
-	old_theme="nord"
-	new_theme="gruvbox-dark"
-fi
+old_theme=$(grep colorscheme ~/.config/nvim/init.vim | cut -d " " -f 2)
+new_theme=$1
 
 sed -i "s/${old_theme}/${new_theme}/g" $(realpath ~/.config/alacritty/alacritty.yml)
 sed -i "s/${old_theme}/${new_theme}/g" $(realpath ~/.config/tmux/tmux.conf)
+sed -i "s/colorscheme ${old_theme}/colorscheme ${new_theme}/g" $(realpath ~/.config/nvim/init.vim)
 
 tmux source-file ~/.config/tmux/tmux-${new_theme}.conf
