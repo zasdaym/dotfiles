b656dedc26e6a4b03f03d74911b74caef6b480dc
Zasda Yusuf Mikail <zasdaym@gmail.com>
2023-11-23 17:29:15 +0700

    Some macOS settings not working via CLI

--- darwin.sh
+++ darwin.sh
@@ -13,51 +13,12 @@ defaults write com.apple.dock show-recents -bool false
 defaults write com.apple.dock tilesize -integer 32
 killall Dock
 
-# Trackpad
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-
-# Fast keyboard repeat rate
-defaults write -g KeyRepeat -int 2
-defaults write -g InitialKeyRepeat -int 15
-defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
-defaults delete -g ApplePressAndHoldEnabled
-
-# Finder
-defaults write -g AppleShowAllExtensions -bool true
-defaults write com.apple.finder FXPreferredViewStyle Nlsv
-defaults write com.apple.finder NewWindowTargetPath -string "file://{$HOME}/"
-defaults write com.apple.Finder AppleShowAllFiles -bool false
-defaults write com.apple.finder ShowStatusBar -bool true
-defaults write com.apple.finder ShowPathbar -bool true
-
-# Show only applications and system prefs on search
-defaults write com.apple.Spotlight orderedItems -array \
-  '{"enabled" = 1;"name" = "APPLICATIONS";}' \
-  '{"enabled" = 1;"name" = "SYSTEM_PREFS";}' \
-  '{"enabled" = 0;"name" = "DIRECTORIES";}' \
-  '{"enabled" = 0;"name" = "PDF";}' \
-  '{"enabled" = 0;"name" = "FONTS";}' \
-  '{"enabled" = 0;"name" = "DOCUMENTS";}' \
-  '{"enabled" = 0;"name" = "MESSAGES";}' \
-  '{"enabled" = 0;"name" = "CONTACT";}' \
-  '{"enabled" = 0;"name" = "EVENT_TODO";}' \
-  '{"enabled" = 0;"name" = "IMAGES";}' \
-  '{"enabled" = 0;"name" = "BOOKMARKS";}' \
-  '{"enabled" = 0;"name" = "MUSIC";}' \
-  '{"enabled" = 0;"name" = "MOVIES";}' \
-  '{"enabled" = 0;"name" = "PRESENTATIONS";}' \
-  '{"enabled" = 0;"name" = "SPREADSHEETS";}' \
-  '{"enabled" = 0;"name" = "SOURCE";}' \
-  '{"enabled" = 0;"name" = "MENU_DEFINITION";}' \
-  '{"enabled" = 0;"name" = "MENU_OTHER";}' \
-  '{"enabled" = 0;"name" = "MENU_CONVERSION";}' \
-  '{"enabled" = 0;"name" = "MENU_EXPRESSION";}' \
-  '{"enabled" = 0;"name" = "MENU_WEBSEARCH";}' \
-  '{"enabled" = 0;"name" = "MENU_SPOTLIGHT_SUGGESTIONS";}'
-
-# Force reload System Preferences
-sudo pkill -1 'System Preferences'
+echo "Enable four fingers swipe"
+echo "Enable two fingers secondary click"
+echo "Enable three finger drag (Accessibility)"
+echo "Show battery percentage in menu bar"
+echo "Set max keyboard repeat rate"
+echo "Set minimal initial keyboard repeat delay"
 
 # Copy fonts
 cp fonts/*.ttf ~/Library/Fonts/
