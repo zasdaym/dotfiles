ead385603fde4938a82738aa84711ca3b15fd31b
Zasda Yusuf Mikail <zasdaym@gmail.com>
2024-12-29 15:53:51 +0700

    Set gruvbox contrast to hard

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,6 +23,11 @@ require('lazy').setup({
     "ellisonleao/gruvbox.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        contrast = "hard",
+      })
+    end,
   },
   {
     "nvim-lualine/lualine.nvim",
