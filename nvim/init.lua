e0cf0deb52cb0d0fe2a9e0c115c1df4398a9632a
Zasda Yusuf Mikail <zasdaym@gmail.com>
2025-01-25 18:32:06 +0700

    Add AI to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -110,10 +110,35 @@ require('lazy').setup({
   },
   {
     "saghen/blink.cmp",
-    dependencies = "rafamadriz/friendly-snippets",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+    },
     version = "*",
     opts = {
       keymap = { preset = "super-tab" },
+    },
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "deepseek",
+      vendors = {
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "https://api.deepseek.com",
+          model = "deepseek-coder",
+        },
+      },
+    },
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
     },
   },
 })
