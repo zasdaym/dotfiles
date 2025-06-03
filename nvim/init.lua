21ec76dedbfdb0546a4c944493cf84f29f83bf47
Zasda Yusuf Mikail <zasdaym@gmail.com>
2025-06-03 14:29:46 +0700

    GitHub theme default

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,7 +19,7 @@ require('lazy').setup({
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme github_light")
+      vim.cmd("colorscheme github_light_default")
     end,
   },
   {
