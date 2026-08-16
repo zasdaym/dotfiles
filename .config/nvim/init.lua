e8d545fd1c609a6a8bcb34fdb4867e1cbf27e884
Zasda Mikail <zasdaym@gmail.com>
2026-08-16 10:56:00 +0700

    Add init.lua comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,4 @@
-vim.o.number = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
-vim.opt.completeopt = { "menuone", "noselect", "fuzzy", "nosort" }
-vim.opt.fillchars:append({ eob = " " })
+# plugins
 
 vim.pack.add({
   { src = "https://github.com/kepano/flexoki-neovim" },
@@ -23,23 +19,31 @@ require("mini.keymap").setup()
 require("mini.pairs").setup()
 require("mini.pick").setup()
 
+# general
+
+vim.cmd.colorscheme("flexoki")
+vim.o.number = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.opt.fillchars:append({ eob = " " })
+vim.lsp.enable({ "gopls", "ty", "yamlls" })
+
+# tab completion
+vim.opt.completeopt = { "menuone", "noselect", "fuzzy", "nosort" }
 MiniKeymap.map_multistep("i", "<Tab>", { "pmenu_next" })
 MiniKeymap.map_multistep("i", "<S-Tab>", { "pmenu_prev" })
 MiniKeymap.map_multistep("i", "<CR>", { "pmenu_accept", "minipairs_cr" })
 MiniKeymap.map_multistep("i", "<BS>", { "minipairs_bs" })
 
+# easy escape
 local escape_modes = { "i", "c", "x", "s" }
 MiniKeymap.map_combo(escape_modes, "jk", "<BS><BS><Esc>")
 MiniKeymap.map_combo(escape_modes, "kj", "<BS><BS><Esc>")
 MiniKeymap.map_combo("t", "jk", "<BS><BS><C-\\><C-n>")
 MiniKeymap.map_combo("t", "kj", "<BS><BS><C-\\><C-n>")
 
-vim.cmd.colorscheme("flexoki")
-
+# keymap
 vim.g.mapleader = " "
-
 vim.keymap.set("n", "<leader>f", function()
   MiniPick.builtin.files({ tool = "git" })
 end, { desc = "Find files" })
-
-vim.lsp.enable({ "gopls", "ty", "yamlls" })
